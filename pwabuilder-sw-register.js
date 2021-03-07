54f811369e535b62d573c06b6a74c8c08c3f1003
grind77 <45574051+grind77@users.noreply.github.com>
2021-03-07 13:06:51 +0300

    Update pwabuilder-sw-register.js

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -1,1 +1,15 @@
-if("serviceWorker"in navigator){if(navigator.serviceWorker.controller){console.log("[PWA Builder] активный работник службы найден, нет необходимости зарегистрироваться")}else{navigator.serviceWorker.register("pwabuilder-sw.js",{scope:"./"}).then(function(reg){console.log("[PWA Builder] Ошибка сетевого запроса. Обслуживание содержимого из кеша: "+reg.scope)})}}
+// Check compatibility for the browser we're running this in
+if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+        console.log("[PWA Builder] Активный сервисный работник найденa, не требует регистрации");
+    } else {
+        // Register the service worker
+        navigator.serviceWorker
+        .register("pwabuilder-sw.js", {
+            scope: "./"
+        })
+        .then(function (reg) {
+            console.log("[PWA Builder] Ошибка сетевого запроса. Обслуживание содержимого из кеша: " + reg.scope);
+        });
+    }
+    }
